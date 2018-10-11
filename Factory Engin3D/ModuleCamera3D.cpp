15a15910dbc19d59fb0db26632160c2912c571b8
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-10-11 19:55:19 +0200

    Look at camera position
    Fps like movement

--- Factory Engin3D/ModuleCamera3D.cpp
+++ Factory Engin3D/ModuleCamera3D.cpp
@@ -68,8 +68,6 @@ update_status ModuleCamera3D::Update(float dt)
 	if (App->input->GetKey(SDL_SCANCODE_LALT) == KEY_REPEAT) LookAt({ 0,0,0 });
 	if (App->input->GetKey(SDL_SCANCODE_I) == KEY_DOWN) LookAt(this->Position);
 
-
-
 	Position += newPos;
 	Reference += newPos;
 
