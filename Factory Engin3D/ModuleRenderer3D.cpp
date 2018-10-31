03392614418451ab1be91aced687cf8bf4bcf365
aleixgab <aleix.gab@gmail.com>
2018-10-31 10:01:17 +0100

    useless code

--- Factory Engin3D/ModuleRenderer3D.cpp
+++ Factory Engin3D/ModuleRenderer3D.cpp
@@ -150,29 +150,6 @@ bool ModuleRenderer3D::Start()
 	// Projection matrix for
 	OnResize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-
-	const int eight = 80;
-	GLubyte checkImage[eight][eight][4];
-	for (int i = 0; i < eight; i++) {
-		for (int j = 0; j < eight; j++) {
-			int c = ((((i & 0x8) == 0) ^ (((j & 0x8)) == 0))) * 255;
-			checkImage[i][j][0] = (GLubyte)c;
-			checkImage[i][j][1] = (GLubyte)c;
-			checkImage[i][j][2] = (GLubyte)c;
-			checkImage[i][j][3] = (GLubyte)255;
-		}
-	}
-
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-	glGenTextures(1, &buffer_id);
-	glBindTexture(GL_TEXTURE_2D, buffer_id);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, eight, eight,
-		0, GL_RGBA, GL_UNSIGNED_BYTE, checkImage);
-
 	return true;
 }
 
