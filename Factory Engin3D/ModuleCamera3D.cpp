a60e8719f82fc091982695cb7c1ca606896a18f3
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-11-01 18:34:24 +0100

    Switching camera to a game object
    Code commentet below to avoid malfunction

--- Factory Engin3D/ModuleCamera3D.cpp
+++ Factory Engin3D/ModuleCamera3D.cpp
@@ -211,3 +211,239 @@ void ModuleCamera3D::CalculateViewMatrix()
 	ViewMatrixInverse = ViewMatrix;
 	ViewMatrixInverse.Inverse();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FUTURE CAMERA
+
+/*#include "Globals.h"
+#include "Application.h"
+#include "ModuleCamera3D.h"
+#include "Light.h"
+#include "MathGeoLib/MathGeoLib.h"
+
+ModuleCamera3D::ModuleCamera3D(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+	X = math::float3(1.0f, 0.0f, 0.0f);
+	Y = math::float3(0.0f, 1.0f, 0.0f);
+	Z = math::float3(0.0f, 0.0f, 1.0f);
+
+	Position = math::float3(0.0f, 0.0f, 5.0f);
+	Reference = math::float3(0.0f, 0.0f, 0.0f);
+
+	CalculateViewMatrix();
+}
+
+ModuleCamera3D::~ModuleCamera3D()
+{}
+
+// -----------------------------------------------------------------
+bool ModuleCamera3D::Start()
+{
+	bool ret = true;
+
+	LOG("Setting up the camera");
+
+	camera = new GameObject(nullptr);
+
+	cameraComponent = (Camera*)camera->AddComponent(ComponentType_CAMERA, nullptr);
+
+	LookAt(float3(0, 0, 0));
+	Look(false);
+
+	return ret;
+}
+
+// -----------------------------------------------------------------
+bool ModuleCamera3D::CleanUp()
+{
+	LOG("Cleaning camera");
+	
+	return true;
+}
+
+// -----------------------------------------------------------------
+update_status ModuleCamera3D::Update(float dt)
+{
+	float speed = cameraSpeed * dt;
+	
+	if (!App->gui->IsAnyWindowHovered())
+	{
+		if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)	speed *= 2.0f;
+
+		if (App->input->GetMouseButton(SDL_BUTTON_RIGHT))
+		{
+			if (App->input->GetKey(SDL_SCANCODE_E) == KEY_REPEAT)
+				camera->transform->position += float3::unitY * speed;
+
+			if (App->input->GetKey(SDL_SCANCODE_Q) == KEY_REPEAT)
+				camera->transform->position -= float3::unitY * speed;
+
+			if (App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT)
+				camera->transform->position += camera->transform->rotation * float3::unitZ * speed;
+
+			if (App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT)
+				camera->transform->position -= camera->transform->rotation * float3::unitZ * speed;
+
+			if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT)
+				camera->transform->position += camera->transform->rotation * float3::unitX * speed;
+
+			if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT)
+				camera->transform->position -= camera->transform->rotation * float3::unitX * speed;
+		}
+
+
+		if (App->input->GetKey(SDL_SCANCODE_F) == KEY_DOWN)
+			Look(App->geometry->GetBBPos(), App->geometry->GetCurrentMeshPivot(), false);
+
+
+		//Zoom with wheel
+		float wheel = (float)App->input->GetMouseZ();
+		if (wheel != 0)
+		{
+			// We do not use dt to zoom with the mouse wheel, the speed at which the mouse wheel rotates is independent of the fps
+			// The movement of the mouse wheel is not a "KEY_REPEAT" every frame but a "KEY_DOWN" occasionally
+			camera->transform->position += camera->transform->rotation * float3::unitZ * wheel * cameraSpeed;
+		}
+
+		if (App->input->GetKey(SDL_SCANCODE_LALT) == KEY_DOWN)
+		{
+			isCameraFocused = true;
+			cameraMovementButton = MouseButton_Left;
+		}
+
+
+		if (isCameraFocused)
+		{
+			LookAt(App->geometry->GetCurrentMeshPivot());
+
+			if (App->input->GetKey(SDL_SCANCODE_LALT) == KEY_UP)
+			{
+				Look(false);
+				isCameraFocused = false;
+				cameraMovementButton = MouseButton_Right;
+			}
+
+			else if (App->input->GetMouseButton(MouseButton_Right) == KEY_REPEAT)
+			{
+				float motion = App->input->GetMouseXMotion() + App->input->GetMouseYMotion();
+				if (motion != 0)
+				{
+					camera->transform->position += camera->transform->rotation * float3::unitZ * motion * dt;
+				}
+			}
+		}
+
+		//Mouse motion ----------------
+
+	   //if(App->input->GetKey(SDL_SCANCODE_LALT) == KEY_REPEAT || App->input->GetKey(SDL_SCANCODE_RALT) == KEY_REPEAT)
+		if (App->input->GetMouseButton(cameraMovementButton) == KEY_REPEAT)
+		{
+			int dx = -App->input->GetMouseXMotion();
+			int dy = -App->input->GetMouseYMotion();
+
+			float sensitivity = 0.01f;
+
+			Position -= Reference;
+
+			if (dx != 0)
+			{
+				float deltaX = (float)dx * sensitivity;
+
+				Quat rotation = Quat::RotateAxisAngle(float3::unitY, deltaX * dt);
+				camera->transform->SetRotation(rotation);
+			}
+
+			if (dy != 0)
+			{
+				float deltaY = (float)dy * sensitivity / 2;
+
+				Quat rotation = Quat::RotateAxisAngle(float3::unitX, deltaY * dt);
+				camera->transform->SetRotation(rotation);
+			}
+
+			Position = Reference + Z * Position.Length();
+		}
+
+	}
+	// Recalculate matrix -------------
+	CalculateViewMatrix();
+
+	return UPDATE_CONTINUE;
+}
+
+// -----------------------------------------------------------------
+void ModuleCamera3D::Look(const float3 &Position, const float3 &Reference, bool RotateAroundReference)
+{
+	camera->transform->position = Position;
+	this->Reference = Reference;
+
+	Z = (Position - Reference).Normalized();
+	X = Cross(math::float3(0.0f, 1.0f, 0.0f), Z).Normalized();
+	Y = Cross(Z, X);
+
+	if (!RotateAroundReference)
+	{
+		this->Reference = this->Position;
+		this->Position += Z * 0.05f;
+	}
+
+	CalculateViewMatrix();
+}
+
+void ModuleCamera3D::Look(bool RotateAroundReference)
+{
+	Look(Position, Reference, RotateAroundReference);
+}
+
+// -----------------------------------------------------------------
+void ModuleCamera3D::LookAt( const float3 &Spot)
+{
+	Reference = Spot;
+
+	Z = (Position - Reference).Normalized();
+	X = Cross(math::float3(0.0f, 1.0f, 0.0f), Z).Normalized();
+	Y = Cross(Z, X);
+
+	CalculateViewMatrix();
+}
+
+
+// -----------------------------------------------------------------
+void ModuleCamera3D::Move(const float3 &Movement)
+{
+	Position += Movement;
+	Reference += Movement;
+
+	CalculateViewMatrix();
+}
+
+// -----------------------------------------------------------------
+float* ModuleCamera3D::GetViewMatrix()
+{
+	return cameraComponent->GetViewMatrix().ptr();
+}
+
+// -----------------------------------------------------------------
+void ModuleCamera3D::CalculateViewMatrix()
+{
+	ViewMatrix = cameraComponent->GetViewMatrix();
+	ViewMatrixInverse = ViewMatrix;
+	ViewMatrixInverse.Inverse();
+}
+*/
--- Factory Engin3D/ModuleCamera3D.h
+++ Factory Engin3D/ModuleCamera3D.h
@@ -44,3 +44,61 @@ private:
 
 	float4x4 ViewMatrix, ViewMatrixInverse;
 };
+
+
+
+
+// FUTURE CAMERA
+
+/*#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "MathGeoLib/Math/float3.h"
+#include "MathGeoLib/Math/float4.h"
+#include "MathGeoLib/Math/float4x4.h"
+
+#include "GameObject.h"
+
+enum MouseButton
+{
+	MouseButton_Left = SDL_BUTTON_LEFT,
+	MouseButton_Middle = SDL_BUTTON_MIDDLE,
+	MouseButton_Right = SDL_BUTTON_RIGHT
+};
+
+class ModuleCamera3D : public Module
+{
+public:
+	ModuleCamera3D(Application* app, bool start_enabled = true);
+	~ModuleCamera3D();
+
+	bool Start();
+	update_status Update(float dt);
+	bool CleanUp();
+
+	void Look(const float3 &Position, const float3 &Reference, bool RotateAroundReference = false);
+	void Look(bool RotateAroundReference);
+	void LookAt(const float3 &Spot);
+	void Move(const float3 &Movement);
+	float* GetViewMatrix();
+
+private:
+
+	void CalculateViewMatrix();
+
+public:
+
+	float3 X, Y, Z, Position, Reference;
+
+private:
+	MouseButton cameraMovementButton = MouseButton_Right;
+	bool isCameraFocused = false;
+
+	float cameraSpeed = 3;
+
+	GameObject* camera = nullptr;
+	Camera* cameraComponent = nullptr;
+
+
+	float4x4 ViewMatrix, ViewMatrixInverse;
+};*/
