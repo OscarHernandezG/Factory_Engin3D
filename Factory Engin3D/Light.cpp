29d20e693c3bde86b7785ac1a8747aee31627166
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-11-01 18:33:49 +0100

    New component camera
    Now implemented in Light component files to avoid conflics with other branches

--- Factory Engin3D/Light.cpp
+++ Factory Engin3D/Light.cpp
@@ -3,6 +3,8 @@
 #include <gl/GL.h>
 //#include <gl/GLU.h>
 
+#include "GameObject.h"
+
 Light::Light() : ref(-1), on(false), position(0.0f, 0.0f, 0.0f)
 {}
 
@@ -39,4 +41,48 @@ void Light::Active(bool active)
 		else
 			glDisable(ref);
 	}
+}
+
+Camera::Camera(GameObject* gameObject) : Component(gameObject)
+{
+	frustum.type = PerspectiveFrustum;
+
+	if (gameObject != nullptr && gameObject->transform != nullptr)
+	{
+		frustum.pos = gameObject->transform->GetPos();
+		frustum.front = gameObject->transform->GetRotation() * float3::unitZ;
+		frustum.up = gameObject->transform->GetRotation() * float3::unitY;
+	}
+	else
+	{
+		frustum.pos = float3::zero;
+		frustum.front = float3::unitZ;
+		frustum.up = float3::unitY;
+	}
+
+	frustum.nearPlaneDistance = 0.0f;
+	frustum.farPlaneDistance = 100.0f;
+	frustum.verticalFov = 60.0f * DEGTORAD;
+	frustum.horizontalFov = 2.f * atanf(tanf(frustum.verticalFov * 0.5f) * (SCREEN_WIDTH / SCREEN_HEIGHT));
+}
+
+
+float4x4 Camera::GetViewMatrix()
+{
+	float4x4 matrix;
+
+	matrix = frustum.ViewMatrix();
+	matrix.Transpose();
+
+	return matrix;
+}
+
+float* Camera::GetProjectionMatrix()
+{
+	float4x4 matrix;
+
+	matrix = frustum.ProjectionMatrix();
+	matrix.Transpose();
+
+	return (float*)matrix.v;
 }
