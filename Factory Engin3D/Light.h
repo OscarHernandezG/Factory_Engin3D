6a3e84430cef83156c8ea39fe2d124edef471682
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-11-01 19:14:00 +0100

    Camera removed from light cpp

--- Factory Engin3D/Light.h
+++ Factory Engin3D/Light.h
@@ -21,45 +21,5 @@ struct Light
 	int ref;
 	bool on;
 };
-
-
-// ------------------------------------------------------------
-// ------------------------------------------------------------
-// ------------------------------------------------------------
-// TEMP CAMERA COMPONENT
-
-
-#include "Component.h"
-
-#include "MathGeoLib/MathGeoLib.h"
-
-class GameObject;
-
-class Camera : public Component
-{
-public:
-
-	Camera(GameObject* gameObject);
-
-	float4x4 GetViewMatrix();
-
-	float* GetProjectionMatrix();
-
-	~Camera() {}
-
-
-	void		SetPos(float x, float y, float z);
-	void		SetRotation(float angle, const float3 &u);
-	void		SetScale(float x, float y, float z);
-	void		SetIdentity();
-
-
-public:
-	Frustum frustum;
-	
-		
-	AABB boundingBox;
-};
-
 #endif
 
