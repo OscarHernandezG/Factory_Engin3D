526e09215e0bc49435226c6ecfea85385e35fb4e
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-11-14 11:53:19 +0100

    Transform do not save BB

--- Factory Engin3D/Transform.cpp
+++ Factory Engin3D/Transform.cpp
@@ -183,102 +183,47 @@ float4x4 Transform::GetLocalMatrix() const
 
 void Transform::SaveComponent(JSON_Object * parent)
 {
-
-	json_object_set_number(parent, "Type", this->type);
-
-	json_object_set_number(parent, "UUID", GetUUID());
-
-	json_object_set_number(parent, "Time Created", GetTime());
-
-	//// Position
-	////------------------------------------------------------------------------
-	JSON_Value* position = json_value_init_object();
-	JSON_Object* positionObj = json_value_get_object(position);
-
-	json_object_set_value(parent, "Position", position);
-
-	json_object_set_number(positionObj, "X", this->position.x);
-	json_object_set_number(positionObj, "Y", this->position.y);
-	json_object_set_number(positionObj, "Z", this->position.z);
-
-	//// Scale
-	////------------------------------------------------------------------------
-	JSON_Value* scale = json_value_init_object();
-	JSON_Object* scalenObj = json_value_get_object(scale);
-
-	json_object_set_value(parent, "Scale", scale);
-
-	json_object_set_number(scalenObj, "X", this->scale.x);
-	json_object_set_number(scalenObj, "Y", this->scale.y);
-	json_object_set_number(scalenObj, "Z", this->scale.z);
-
-	//// Rotation
-	////------------------------------------------------------------------------
-	JSON_Value* rotation = json_value_init_object();
-	JSON_Object* rotationObj = json_value_get_object(rotation);
-
-	json_object_set_value(parent, "Rotation", rotation);
-
-
-	json_object_set_number(rotationObj, "X", this->rotation.x);
-	json_object_set_number(rotationObj, "Y", this->rotation.y);
-	json_object_set_number(rotationObj, "Z", this->rotation.z);
-	json_object_set_number(rotationObj, "W", this->rotation.w);
+																			  
+	json_object_set_number(parent, "Type", this->type);						  
+																			  
+	json_object_set_number(parent, "UUID", GetUUID());						  
+																			  
+	json_object_set_number(parent, "Time Created", GetTime());				  
+																			  
+	//// Position															  
+	////----------------------------------------------------------------------
+	JSON_Value* position = json_value_init_object();						  
+	JSON_Object* positionObj = json_value_get_object(position);				  
+																			  
+	json_object_set_value(parent, "Position", position);					  
+																			  
+	json_object_set_number(positionObj, "X", this->position.x);				  
+	json_object_set_number(positionObj, "Y", this->position.y);				  
+	json_object_set_number(positionObj, "Z", this->position.z);				  
+																			  
+	//// Scale																  
+	////----------------------------------------------------------------------
+	JSON_Value* scale = json_value_init_object();							  
+	JSON_Object* scalenObj = json_value_get_object(scale);					  
+																			  
+	json_object_set_value(parent, "Scale", scale);							  
+																			  
+	json_object_set_number(scalenObj, "X", this->scale.x);					  
+	json_object_set_number(scalenObj, "Y", this->scale.y);					  
+	json_object_set_number(scalenObj, "Z", this->scale.z);					  
+																			  
+	//// Rotation															  
+	////----------------------------------------------------------------------
+	JSON_Value* rotation = json_value_init_object();						  
+	JSON_Object* rotationObj = json_value_get_object(rotation);				  
+																			  
+	json_object_set_value(parent, "Rotation", rotation);					  
+																			  
+																			  
+	json_object_set_number(rotationObj, "X", this->rotation.x);				  
+	json_object_set_number(rotationObj, "Y", this->rotation.y);				  
+	json_object_set_number(rotationObj, "Z", this->rotation.z);				  
+	json_object_set_number(rotationObj, "W", this->rotation.w);				  
 	//------------------------------------------------------------------------
-
-	// Bounding box
-	//------------------------------------------------------------------------
-	JSON_Value* boundingBoxV = json_value_init_object();
-	JSON_Object* boundingBoxObj = json_value_get_object(boundingBoxV);
-
-	json_object_set_value(parent, "Bounding Box", boundingBoxV);
-	{
-		// Min point
-		JSON_Value* minBB = json_value_init_object();
-		JSON_Object* minBBo = json_value_get_object(minBB);
-
-		json_object_set_value(parent, "Min", minBB);
-
-		json_object_set_number(minBBo, "X", boundingBox.minPoint.x);
-		json_object_set_number(minBBo, "Y", boundingBox.minPoint.y);
-		json_object_set_number(minBBo, "Z", boundingBox.minPoint.z);
-
-		// Max point
-		JSON_Value* maxBB = json_value_init_object();
-		JSON_Object* maxBBo = json_value_get_object(maxBB);
-
-		json_object_set_value(parent, "max", maxBB);
-
-		json_object_set_number(maxBBo, "X", boundingBox.maxPoint.x);
-		json_object_set_number(maxBBo, "Y", boundingBox.maxPoint.y);
-		json_object_set_number(maxBBo, "Z", boundingBox.maxPoint.z);
-	}
-	// Original Bounding box
-	//------------------------------------------------------------------------
-	JSON_Value* originalBB = json_value_init_object();
-	JSON_Object* originalBBObj = json_value_get_object(originalBB);
-
-	json_object_set_value(parent, "Original Bounding Box", originalBB);
-	{
-		// Min point
-		JSON_Value* minOBB = json_value_init_object();
-		JSON_Object* minOBBo = json_value_get_object(minOBB);
-
-		json_object_set_value(parent, "Min", minOBB);
-
-		json_object_set_number(minOBBo, "X", originalBoundingBox.minPoint.x);
-		json_object_set_number(minOBBo, "Y", originalBoundingBox.minPoint.y);
-		json_object_set_number(minOBBo, "Z", originalBoundingBox.minPoint.z);
-
-		// Max point
-		JSON_Value* maxOBB = json_value_init_object();
-		JSON_Object* maxOBBo = json_value_get_object(maxOBB);
-
-		json_object_set_value(parent, "max", maxOBB);
-
-		json_object_set_number(maxOBBo, "X", originalBoundingBox.maxPoint.x);
-		json_object_set_number(maxOBBo, "Y", originalBoundingBox.maxPoint.y);
-		json_object_set_number(maxOBBo, "Z", originalBoundingBox.maxPoint.z);
-	}
 }
 
