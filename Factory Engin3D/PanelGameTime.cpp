5f992efea92d25745aef4bd25230d5cad4ca05d6
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-12-20 14:53:34 +0100

    Start timers onPlay

--- Factory Engin3D/PanelGameTime.cpp
+++ Factory Engin3D/PanelGameTime.cpp
@@ -1,13 +1,25 @@
 #include "Application.h"
 #include "ModuleImGui.h"
+#include "ComponentEmitter.h"
 
 void ModuleImGui::CreateGameTime()
 {
 	if (ImGui::Begin("GameSimulate", &gameTimeWindow, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoResize))
 	{
 		if (ImGui::Button("Play", { 50,25 }))
+		{
 			App->time->gameState = GameState_PLAYING;
-		
+
+			if (App->geometry->currentGameObject)
+			{
+				ComponentEmitter* emitter = (ComponentEmitter*)App->geometry->currentGameObject->GetComponent(ComponentType_EMITTER);
+				if (emitter)
+				{
+					emitter->StartEmitter();
+				}
+			}
+		}
+
 		ImGui::SameLine();
 
 		if (ImGui::Button("Pause", { 50,25 }))
